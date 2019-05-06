a02033069b5961dea4f1c1f2e29bd9d8c939b2d6
hanbit10 <38600431+hanbit10@users.noreply.github.com>
2019-05-06 12:40:08 +0200

    hanbit

--- Blatt02/listen.c
+++ Blatt02/listen.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Datentyp definiert
 typedef struct stud_type {
     int matnum;
     char vorname[20];
@@ -11,15 +12,18 @@ typedef struct stud_type {
     struct stud_type *next;
 } stud_type;
 
+int const SMALLER = -1;
+int const BIGGER = 1;
 
 /* Ist die Datenbank leer?      */
+//es wird geprueft, ob der Pointer eine Adresse enthaelt oder nicht.
 bool is_empty(stud_type const* liste) {
     return !liste;
 }
 
 
 /* Einfuegen eines Elementes
- * 
+ *
  * Bekommt einen Zeiger auf einen Zeiger der auf das 1. Element der Liste zeigt
  * Bekommt MatNr, Vorname und Nachname des Studenten der eingefügt werden soll
  *
@@ -27,18 +31,65 @@ bool is_empty(stud_type const* liste) {
  * Gibt false für einen Fehler zurück
  */
 bool enqueue(stud_type** studenten_liste, int matnum, char const vorname[20], char const nachname[20]) {
-    /* Hole dynamischen Speicher für den neuen Listen Eintrag */
-    
-    /* Befülle den Speicher (vorsicht vor buffer overflows!) */
-    
+        /* Hole dynamischen Speicher für den neuen Listen Eintrag */
+    stud_type * new_node = malloc(sizeof(stud_type));
+   /* Befülle den Speicher (vorsicht vor buffer overflows!) */
+
+   new_node->matnum = matnum;
+   // node->vorname[20] = vorname[20];
+   // node->nachname[20] = nachname[20];
+   for (int i = 0; i<20; i++) {
+      new_node->vorname[i] = vorname[i];
+      new_node->nachname[i] = nachname[i];
+   }
+   new_node->next = NULL;
+   // the new node is inserted before the existing node
+   // node->next = *studenten_liste;
+
     /* Füge den neuen Eintrag in die Liste ein */
         /* Ist die Liste leer ? */
+   if (is_empty(*studenten_liste) == true) {
+      // the new node becomes the first node in the list
+      *studenten_liste = new_node;
+   }
+   /* Sortier den Studenten aufsteigend nach Matrikelnummer ein (matrikelnummern sind einzigartig) */
+   else {
+      stud_type* curr_node = *studenten_liste;
 
-        /* Sortier den Studenten aufsteigend nach Matrikelnummer ein (matrikelnummern sind einzigartig) */
+      if (curr_node->matnum == new_node->matnum) {
+         //jede Matrikelnummer sollte eindeutig sein
+         return false;
+      }
+      if (curr_node->matnum > new_node->matnum) {
+         //der neu eingefuegte Knoten wird als erstes Element
+         new_node->next = curr_node;
+         *studenten_liste = new_node;
+      }
+      else {
+         while (curr_node->next) {
+            if (curr_node->next->matnum == new_node->matnum) {
+               //jede Matrikelnummer sollte eindeutig sein
+               return false;
+            }
+            if (curr_node->next->matnum > new_node->matnum) {
+               stud_type* temp = curr_node->next;
+               curr_node->next = new_node;
+               new_node->next = temp;
+               return true;
+            }
+            curr_node = curr_node->next;
+         }
+         curr_node->next = new_node;
+      }
+      return true;
+   }
+   return true;
 }
 
-/* Löschen eines Elementes. 
- * 
+
+
+/* Löschen eines Elementes.
+ *
  * Bekommt einen Zeiger auf einen Zeiger der auf das 1. Element der Liste zeigt
  * Bekommt die MatNr des Studenten der zu löschen ist
  *
@@ -47,18 +98,146 @@ bool enqueue(stud_type** studenten_liste, int matnum, char const vorname[20], ch
  */
 bool dequeue(stud_type** studenten_liste, int matnum) {
     /* Prüfe Randbedingungen */
-    
-    /* Finde den Studenten */
-    
+
+    // 1.Fall : die Liste ist leer
+    if(*studenten_liste == NULL){
+        return false;
+    }
+    // 2.Fall : das zu loeschende Element ist das erste Element von dem Array
+    stud_type* currStud = *studenten_liste;
+    if(currStud->matnum == matnum){
+        *studenten_liste = currStud->next;
+        return true;
+    }
+    // 3.Fall : das zuloeschende Element ist im Mittel des Arrays
     /* Lösche den Studenten und gibt den Speicher frei */
-    
-    /* Was muss passieren wenn das 1. Element gelöscht wird? */
+    stud_type* target = NULL;
+    stud_type* previous;
+    while(currStud){
+        if(currStud->matnum == matnum){
+            target = currStud;
+            previous->next = currStud->next;
+            free(target);
+            return true;
+        }
+        previous = currStud;
+        currStud = currStud->next;
+    }
+    /* Was muss passieren wenn das 1. Element gelöscht wird?
+     * -> es wurde im 2. Fall behandelt.
+     */
+
     /* Was ist wenn es nicht in der Liste ist? */
+    if(target == NULL){
+        return false;
+    }
     /* ... */
-    
+    return true;
 }
 
-/* Auslesen eines Elementes 
+int compareFirstname(stud_type const* a, stud_type const* b){
+    int i;
+    int aF;
+    int bF;
+    for( i = 0; i<20; i++){
+        aF = a->vorname[i];
+        bF = b->vorname[i];
+
+        if(aF == 0){
+            return -1;
+        }
+        else if(bF == 0){
+            return 1;
+        }
+        //gibt -1 zurueck, wenn der Nachname von dem ersten Parameter vorne als der vom zweiten Parameter
+        else if(aF < bF){
+            return -1;
+        }
+        //andersum
+        else if (aF > bF)
+        {
+            return 1;
+        }
+    }
+    //gibt 0 zurueck, wenn die Namen identisch sind
+    return 0;
+}
+
+int compareLastname(stud_type const* a, stud_type const* b){
+    int i;
+    int aF;
+    int bF;
+    for( i = 0; i<20; i++){
+        aF = a->nachname[i];
+        bF = b->nachname[i];
+
+        if(aF == 0){
+            return -1;
+        }
+        else if(bF == 0){
+            return 1;
+        }
+        //gibt -1 zurueck, wenn der Nachname von dem ersten Parameter vorne als der vom zweiten Parameter
+        else if(aF < bF){
+            return -1;
+        }
+        //andersum
+        else if (aF > bF)
+        {
+            return 1;
+        }
+    }
+    //gibt 0 zurueck, wenn die Namen identisch sind
+    return 0;
+}
+
+
+
+
+stud_type** sortieren(int (*compare)(stud_type const* a, stud_type const* b), stud_type** studenten_liste){
+    stud_type** newList = malloc(sizeof(stud_type));
+
+    if(*studenten_liste == NULL){
+        return newList;
+    }
+    else{
+        while(*studenten_liste != NULL){
+            stud_type* currStud = *studenten_liste;
+            stud_type* target = currStud;
+            while(currStud){
+                if(compare(target, currStud) == BIGGER){
+                    target = currStud;
+                }
+                currStud = currStud->next;
+            }
+            if(*newList == NULL){
+                //*newList = target;
+                enqueue(newList, target->matnum, target->vorname, target->nachname);
+            }else{
+                stud_type* tmp = *newList;
+                while(tmp->next){
+                    tmp = tmp->next;
+                }
+
+                stud_type* tmp2 = malloc(sizeof(stud_type));
+                tmp2->matnum = target->matnum;
+                for(int i = 0; i<20; i++){
+                    tmp2->vorname[i] = target->vorname[i];
+                    tmp2->nachname[i] = target->nachname[i];
+                }
+                tmp2->next = NULL;
+
+                tmp->next = tmp2;
+            }
+
+        dequeue(studenten_liste, target->matnum);
+        }
+
+    }
+    return newList;
+}
+
+/* Auslesen eines Elementes
  *
  * Bekommt pointer auf den Listenstart
  * Bekommt MatNr des Studenten der ausgelesen werden soll
@@ -101,7 +280,7 @@ static void test_get(stud_type const* liste, int matnum) {
     char vorname[20];
     char nachname[20];
     if(get_student(liste, matnum, vorname, nachname)) {
-        printf("    Matrikelnummer %4i: %s %s\n", matnum, vorname, nachname);
+        printf("    (**)Matrikelnummer %4i: %s %s\n", matnum, vorname, nachname);
     } else {
         printf("    Matrikelnummer %4i ist unbekannt\n", matnum);
     }
@@ -154,6 +333,13 @@ int main(void) {
     test_dequeue(&studenten_liste, 5678);
     test_enqueue(&studenten_liste, 1, "Tywin", "Lannister");
     test_dump(studenten_liste);
+
+
+
+    stud_type** vorname_liste = sortieren(compareFirstname, &studenten_liste);
+    test_dump(*vorname_liste);
+    stud_type** nachname_list = sortieren(compareLastname, vorname_liste);
+    test_dump(*nachname_list);
 
     {
         /* Erzeuge sortierte Liste nach Vorname */
