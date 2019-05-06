0eeacd11f47e0d79046f28c4d2cc537541daf6c2
hanbit10 <38600431+hanbit10@users.noreply.github.com>
2019-05-06 15:51:11 +0200

    hanbit

--- Blatt02/parent.c
+++ Blatt02/parent.c
@@ -3,19 +3,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <spawn.h>
+#include <sys/wait.h>
 
 extern char **environ; // standard libc process environment
 
 int main(void) {
   pid_t child1;
+  pid_t child2;
   char const* args[] = { "./child1", NULL };
+  char const* args2[] = { "./child2", NULL };
+
   int ret = posix_spawn(&child1, *args, NULL, NULL, (char* const*)args, environ);
+  int ret2 = posix_spawn(&child2, *args2, NULL, NULL, (char* const*)args2, environ);
   if(ret != 0) {
     fprintf(stderr, "Could not start child process!\n");
     abort();
-  }
+  }else if(ret == 0){
+	printf("child1: %i\n", child1);
+	if(ret2 == 0){
+	printf("child1: %i\n", child2);
+	}
+ }
 
-  while(true) {
-    write(fileno(stdout), "B", 1);
-  }
+
+  /*while(true) {
+    //write(fileno(stdout), "B", 1);
+    return 0;
+  }*/
 }
